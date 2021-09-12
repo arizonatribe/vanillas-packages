2b75707e54130c50820e5812cb4523aa20dc6e5d
David Nunez <arizonatribe@gmail.com>
2021-09-12 19:36:01 +0000

    Fixed an issue with the parseArgs() helper not handling arg val order as expected. Also added some path'ing helpers to assist with multi-package repository management

--- packages/cli-toolkit/index.d.ts
+++ packages/cli-toolkit/index.d.ts
@@ -27,3 +27,45 @@ export function parseArgs(args: string[] | number[] | boolean[]): ParsedArgs
  * @returns {ArgValue} The original value, but coerced to a number or boolean _if_ that value was a stringified number or boolean
  */
 export function toArgValue(arg: string | number | boolean): ArgValue
+
+/**
+ * Locates the first path which exists from a list of possible file paths.
+ * Will fall back to `process.cwd()` as its base directory for any relative paths.
+ *
+ * @function
+ * @name findPackagesFolder
+ * @param {Array<string>} possibleLocations A list of either relative or absolute file paths which may exist
+ * @param {string} [baseDir=process.cwd()] The base directory from which to resolve any relative file paths
+ * @returns {string|undefined} The first path that exists
+ */
+export function findFirstWhichExists(possibleLocations: string[], baseDir?: string): string | undefined
+
+/**
+ * Locates the (absolute) path for the packages folder in a multi-package repository
+ *
+ * @function
+ * @name findPackagesFolder
+ * @param {string} [baseDir] An optional override starting path for resolving the packages folder (otherwise will start from this current directory and traverse upwards to several common, likely locations for the packages)
+ * @returns {string|undefined} The path to the packages folder in a multi-package repo (if this is one)
+ */
+export function findPackagesFolder(baseDir?: string): string | undefined 
+
+/**
+ * Locates the (absolute) paths for all the packages in a multi-package repo which are TypeScript packages (specifically to their tsconfig.json manifests)
+ *
+ * @function
+ * @name findTsConfigs
+ * @param {string} [baseDir] An optional override starting path for resolving the packages folder (otherwise will start from this current directory and traverse upwards to several common, likely locations for the packages)
+ * @returns {Array<string>|undefined} The paths to the tsconfig.json files for all the TypeScript packages
+ */
+export function findTsConfigs(baseDir?: string): string[] | undefined
+
+/**
+ * Locates the (absolute) path tsconfig.json file for this current project into which this base eslint config has been installed as one of its devDependencies
+ *
+ * @function
+ * @name findCurrentProjectTsConfig
+ * @param {string} [baseDir] An optional override starting path for resolving the packages folder (otherwise will start from this current directory and traverse upwards to several common, likely locations for the packages)
+ * @returns {string|undefined} The path to the tsconfig.json file for this current project package
+ */
+export function findCurrentProjectTsConfig(baseDir?: string): string | undefined
