b4551b83ceba48b03b179698279de8479037482e
David Nunez <arizonatribe@gmail.com>
2021-09-12 19:36:01 +0000

    Fixed an issue with the parseArgs() helper not handling arg val order as expected. Also added some path'ing helpers to assist with multi-package repository management

---
+++ packages/cli-toolkit/lib/args.js
@@ -0,0 +1,84 @@
+const toCamelCase = require("vanillas/toCamelCase")
+
+/**
+ * Coerces a given value into a boolean or number (if appropriate) otherwise returns the value as-is
+ *
+ * @function
+ * @name toArgValue
+ * @param {string|boolean|number|Array<*>|object} val A value of any type
+ * @returns {boolean|number|string|Array<*>|object} The original value, but coerced to a number or boolean _if_ that value was a stringified number or boolean
+ */
+function toArgValue(val) {
+  const primitiveValue = /^(true|false)$/i.test(val)
+    ? /^true$/i.test(val)
+    : /^\d+(\.\d+)?$/.test(val)
+      ? +val
+      : undefined
+  if (primitiveValue != null) {
+    return primitiveValue
+  }
+
+  try {
+    return JSON.parse(val)
+  } catch (err) {
+    return /^([^,]+),([^,]+)/.test(val) ? val.split(",") : val
+  }
+}
+
+/**
+ * Parses command-line flags into a single object of camel-cased key/val pairs
+ *
+ * @function
+ * @name parseArgs
+ * @param {Array<*>} args Command-line args (will default to `process.argv.slice(2)` if nothing is provided)
+ * @returns {object} The parsed command-line flags
+ */
+function parseArgs(args) {
+  const argv = Array.isArray(args)
+    ? [...args]
+    : process.argv.slice(2)
+  const len = argv.length
+
+  let lastArgName
+  const options = {}
+
+  for (let i = 0; i < len; i++) {
+    const arg = argv[i]
+
+    if (/^--?/.test(arg)) {
+      const [key, val = true] = toCamelCase(arg.replace(/^--?/, "")).split("=")
+
+      options[key] = toArgValue(val)
+
+      if (/^([^=]+)=([^=]+)/.test(arg)) {
+        lastArgName = undefined
+      } else {
+        lastArgName = key
+      }
+    } else {
+      const argVal = toArgValue(arg)
+      const argName = lastArgName || "_"
+      options[argName] = options[argName] == null
+        ? argVal
+        : Array.isArray(options[argName])
+          ? [...(options[argName]), argVal]
+          : options[argName] === true
+            ? (Array.isArray(argVal) ? argVal : [argVal])
+            : [options[argName], argVal]
+      lastArgName = undefined
+    }
+  }
+
+  Object.keys(options)
+    .filter(k => options[k] === undefined)
+    .forEach(key => {
+      options[key] = true
+    })
+
+  return options
+}
+
+module.exports = {
+  toArgValue,
+  parseArgs
+}
---
+++ packages/cli-toolkit/lib/paths.js
@@ -0,0 +1,124 @@
+const fs = require("fs")
+const path = require("path")
+
+/**
+ * Locates the first path which exists from a list of possible file paths.
+ * Will fall back to `process.cwd()` as its base directory for any relative paths.
+ *
+ * @function
+ * @name findPackagesFolder
+ * @param {Array<string>} possibleLocations A list of either relative or absolute file paths which may exist
+ * @param {string} [baseDir=process.cwd()] The base directory from which to resolve any relative file paths
+ * @returns {string|undefined} The first path that exists
+ */
+function findFirstWhichExists(possibleLocations, baseDir) {
+  return (Array.isArray(possibleLocations) ? possibleLocations : [])
+    .filter(loc => typeof loc === "string")
+    .map(loc => (/^\//.test(loc) ? loc : path.resolve(baseDir || process.cwd(), loc)))
+    .find(loc => fs.existsSync(loc))
+}
+
+/**
+ * Locates the (absolute) path for the packages folder in a multi-package repository
+ *
+ * @function
+ * @name findPackagesFolder
+ * @param {string} [baseDir] An optional override starting path for resolving the packages folder (otherwise will start from this current directory and traverse upwards to several common, likely locations for the packages)
+ * @returns {string|undefined} The path to the packages folder in a multi-package repo (if this is one)
+ */
+function findPackagesFolder(baseDir) {
+  const possiblePackagesRootLocations = [
+    /* A standard multi-package repository where packages are in a root packages/ or workspaces/ directory off the root of the repo */
+    "../../../packages",
+    "../../../workspaces",
+
+    /* When this is installed into node_modules/@scope/eslint-config/ inside of one of the non-hoisted packages off the root packages/ or workspaces/ directory */
+    "../../../../../../packages",
+    "../../../../../../workspaces",
+
+    /* When this is installed into node_modules/@scope/eslint-config/ but not hoisted (qnd this is run from that package root as a process.cwd()) */
+    "../../../../../packages",
+    "../../../../../workspaces",
+
+    /* When this is installed into node_modules/@scope/eslint-config/ but hoisted to the top-level node_modules/ off the root */
+    "../../../../packages",
+    "../../../../workspaces",
+
+    /* When the baseDir is supplied and it is the root of one of the repos in a multi-package repo */
+    "../../packages",
+    "../../workspaces",
+
+    /* When the baseDir is supplied and it is the root of the multi-package
+     * repo */
+    "./packages",
+    "./workspaces"
+  ]
+
+  return findFirstWhichExists(possiblePackagesRootLocations, baseDir)
+}
+
+/**
+ * Locates the (absolute) paths for all the packages in a multi-package repo which are TypeScript packages (specifically to their tsconfig.json manifests)
+ *
+ * @function
+ * @name findTsConfigs
+ * @param {string} [baseDir] An optional override starting path for resolving the packages folder (otherwise will start from this current directory and traverse upwards to several common, likely locations for the packages)
+ * @returns {Array<string>|undefined} The paths to the tsconfig.json files for all the TypeScript packages
+ */
+function findTsConfigs(baseDir) {
+  const packagesFolder = findPackagesFolder(baseDir)
+  if (packagesFolder) {
+    const tsConfigPaths = fs.readdirSync(packagesFolder)
+      .map(pkgFolder => path.resolve(packagesFolder, pkgFolder, "tsconfig.json"))
+      .filter(tsConfigPath => fs.existsSync(tsConfigPath))
+
+    return tsConfigPaths.length ? tsConfigPaths : undefined
+  }
+
+  return undefined
+}
+
+/**
+ * Locates the (absolute) path tsconfig.json file for this current project into which this base eslint config has been installed as one of its devDependencies
+ *
+ * @function
+ * @name findCurrentProjectTsConfig
+ * @param {string} [baseDir] An optional override starting path for resolving the packages folder (otherwise will start from this current directory and traverse upwards to several common, likely locations for the packages)
+ * @returns {string|undefined} The path to the tsconfig.json file for this current project package
+ */
+function findCurrentProjectTsConfig(baseDir) {
+  const possibleTsConfigLocations = [
+    /* When the baseDir is supplied and it is the root of the repo */
+    "./tsconfig.eslint.json",
+    "./tsconfig.json",
+    "./tsconfig.build.json",
+
+    /* When this is installed into node_modules/@scope/eslint-config/ of a
+* dependent package/project */
+    "../../../../tsconfig.eslint.json",
+    "../../../../tsconfig.json",
+    "../../../../tsconfig.build.json",
+
+    /* Looks for the tsconfig.json at the root of a multi-package repo in
+* which this current package lives */
+    "../../../tsconfig.eslint.json",
+    "../../../tsconfig.json",
+    "../../../tsconfig.build.json",
+
+    /* When this is installed into node_modules/@scope/eslint-config/
+     * inside of one of the non-hoisted packages off the root packages/ or
+     * workspaces/ directory */
+    "../../../../../../tsconfig.eslint.json",
+    "../../../../../../tsconfig.json",
+    "../../../../../../tsconfig.build.json"
+  ]
+
+  return findFirstWhichExists(possibleTsConfigLocations, baseDir)
+}
+
+module.exports = {
+  findCurrentProjectTsConfig,
+  findFirstWhichExists,
+  findPackagesFolder,
+  findTsConfigs
+}
