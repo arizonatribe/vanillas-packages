245c3eb31db8b2504fdcbe46cd5e045a5cd038f9
David Nunez <arizonatribe@gmail.com>
2021-11-01 03:25:20 +0000

    feat: added helpers to force copy a directory and to lift a nested packages sub-folder to the top of the repo

---
+++ packages/cli-toolkit/lib/fs.js
@@ -0,0 +1,69 @@
+const fs = require("fs-extra")
+const path = require("path")
+const { resolveGlobIfExists, resolvePathIfExists } = require("./paths")
+
+/**
+ * Copies a source folder to a destination path (overwriting anything already existing at the destination)
+ *
+ * @function
+ * @name copyFolder
+ * @param {string} srcPath The source folder to be copied
+ * @param {string} destPath The desination folder where the source folder is being copied to
+ * @param {string} [baseDir=process.cwd()] The base directory from which to resolve any relative file paths
+ * @returns {string} The full (absolute) desination path where the copying completed
+ */
+function copyFolder(srcPath, destPath, baseDir) {
+  const resolvedPath = resolvePathIfExists(srcPath, baseDir)
+  if (fs.statSync(resolvedPath).isDirectory()) {
+    fs.copySync(resolvedPath, destPath, { overwrite: true })
+    return resolvePathIfExists(destPath, baseDir)
+  }
+  return undefined
+}
+
+/**
+ * Copies a folder nested nested inside one or more packages folders
+ *
+ * @function
+ * @name copyNestedPackagesFolder
+ * @throws {Error} When the packages folder doesn't exist
+ * @throws {Error} When no nested folders are found in the packages folder
+ * @param {string} [packagesFolder=packages] The packages sub-folder name (defaults to ./packages)
+ * @param {string} [nestedFolder=docs] The folder nested across one or more packages folders
+ * @param {string} [baseDir=process.cwd()] The base directory from which to resolve any relative file paths
+ * @returns {Array<string>} The list of copied (absolute) paths
+ */
+function copyNestedPackagesFolder(packagesFolder = "packages", nestedFolder = "docs", baseDir) {
+  if (!nestedFolder || /^\s*$/.test(nestedFolder)) {
+    throw new Error("Missing the nested folder")
+  }
+
+  const existingPackagesFolder = resolvePathIfExists(packagesFolder, baseDir)
+
+  if (!existingPackagesFolder) {
+    throw new Error(`Unable to find packages at: ${packagesFolder}`)
+  }
+
+  const pattern = `${packagesFolder.replace(/\/$/, "")}/*/${nestedFolder}`
+  const absFolders = resolveGlobIfExists(pattern, baseDir)
+    .map(f => f.replace(new RegExp(`${existingPackagesFolder}\\/?/`), ""))
+
+  if (!absFolders.length) {
+    throw new Error(`Unable to find ${nestedFolder} folders nested in the packages folder via: ${pattern}`)
+  }
+
+  const copiedPaths = []
+
+  absFolders.forEach(docsPath => {
+    const newPath = path.resolve(baseDir || process.cwd(), nestedFolder, docsPath.split(path.sep)[0])
+    copiedPaths.push(newPath)
+    copyFolder(path.resolve(existingPackagesFolder, docsPath), newPath, baseDir)
+  })
+
+  return copiedPaths
+}
+
+module.exports = {
+  copyFolder,
+  copyNestedPackagesFolder
+}
--- packages/cli-toolkit/lib/index.js
+++ packages/cli-toolkit/lib/index.js
@@ -1,6 +1,7 @@
+const fs = require("./fs")
 const git = require("./git")
 const args = require("./args")
 const paths = require("./paths")
 const scaffold = require("./scaffold")
 
-module.exports = { ...args, ...paths, ...scaffold, ...git }
+module.exports = { ...args, ...paths, ...scaffold, ...git, ...fs }
